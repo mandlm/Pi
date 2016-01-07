fdf61c5d3a4889eeea818b7ce07b06c5437e6438
Michael Mandl <mandlm@molez.net>
2016-01-07 20:51:49 +0100

    Refactoring

--- Moped/UserLed.py
+++ Moped/UserLed.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+﻿#!/usr/bin/python
 
 import ConfigParser
 import RPi.GPIO as GPIO
@@ -7,27 +7,34 @@ yellowLed = 15
 redLed = 17
 greenLed = 18
 
+mopidyConf = '/etc/mopidy/mopidy.conf'
+
 def setLeds(yellow, red, green):
     GPIO.output(yellowLed, yellow)
     GPIO.output(redLed, red)
     GPIO.output(greenLed, green)
 
-config = ConfigParser.ConfigParser()
-config.read('/etc/mopidy/mopidy.conf')
+def getConfiguredLedColor():
+	config = ConfigParser.ConfigParser()
+	config.read(mopidyConf)
 
-ledColor = config.get('moped-switcher', 'led')
+	return config.get('moped-switcher', 'led')
+
+def setConfiguredLedColor():
+	ledColor = getConfiguredLedColor()
+
+	if ledColor == 'yellow':
+		setLeds(1, 0, 0)
+	elif ledColor == 'red':
+		setLeds(0, 1, 0)
+	elif ledColor == 'green':
+		setLeds(0, 0, 1)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(yellowLed, GPIO.OUT)
 GPIO.setup(redLed, GPIO.OUT)
 GPIO.setup(greenLed, GPIO.OUT)
 
-if ledColor == 'yellow':
-    print 'yellow'
-    setLeds(1, 0, 0)
-elif ledColor == 'red':
-    setLeds(0, 1, 0)
-elif ledColor == 'green':
-    setLeds(0, 0, 1)
+setConfiguredLedColor()
 
 GPIO.cleanup()
